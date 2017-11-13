1fd7c50b87d9724c5e3a65f0dfcab4bf2dba1cc9
Unknown <a.truchon86@gmail.com>
2017-11-13 15:10:17 +0100

    Ajout css

--- tchat.js
+++ tchat.js
@@ -1,16 +1,82 @@
-function createUser() {
+var authKey = "";
+
+function createUser(){
+  $.ajax({
+    url: 'http://messenger.api.niamor.com/createUser'
+  }).done(CUse);
+}
+  
+var myUser;
+
+function CUse(user){
+  myUser = user;
+}
+createUser();
+
+
+
+
+function sendMessage(){
+	
+	changePseudo();
+	console.log(myUser.username);
+  $message = document.getElementById('chatText').value;
+  $.ajax({
+    url: 'http://messenger.api.niamor.com/sendMessage',
+        method: 'post',
+        data: {
+          authKey: myUser.authKey,
+          text: $message,
+          to: 0
+        }
+        
+  })
+}
+
+var othUse;
+
+function chatBox(){
+	//monButton();
+  $.ajax({
+    url: 'http://messenger.api.niamor.com/getMessages',
+    method: 'post',
+    data: {
+      authKey: myUser.authKey,
+      lastId: 0
+    }
+
+  }).done(affichMess)
+
+
+}
+
+function affichMess(oUse){
+  document.getElementById('chatBox').innerHTML = "";
+  
+for (i = 0; i < oUse.length; i++) {
+  othUse = oUse[i];
+  document.getElementById('chatBox').innerHTML += othUse.from.username+" : "+othUse.text+"<br>";
+  }
+}
+
+$(document).ready(setInterval(chatBox, 3000));
+//$(document).ready(chatBox);
+
+
+function changePseudo(){
+	var recupPseudo = document.getElementById('pseudo').value;
+	
 	$.ajax({
-		url: 'http://messenger.api.niamor.com/createUser',
-		method: 'post',
-	}).done(utilisateurCree);
+		url:'http://messenger.api.niamor.com/changeUsername',
+		method : 'post',
+		data: {
+		authKey : myUser.authKey,
+		username : recupPseudo}
+	}).done(changementLocal);
+	console.log(myUser.authKey);
 }
 
-function utilisateurCree(user) {
-	myUser = user;
-	console.log(user);
-}
+function changementLocal(change){
 
-// 	for ( i = 0 ; i < user.length ; i++ ) {
-// 		createUser(user[i]);
-// 		lastId = user[i].id;
-// }
+	console.log(change);
+}
